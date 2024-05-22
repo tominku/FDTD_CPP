23ca74cc0c21a4a044bd695776d3dafa4e0c9cde
tominku <tominku@gmail.com>
2024-05-22 16:24:59 +0900

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
+// OpenMP header
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    std::cout << "FDTD C++ project!" << std::endl;
+int main()
+{
+	int nthreads, tid;
+
+	// Begin of parallel region
+	#pragma omp parallel private(nthreads, tid)
+	{
+		// Getting thread number
+		tid = omp_get_thread_num();
+		printf("Welcome to GFG from thread = %d\n",
+			tid);
+
+		if (tid == 0) {
+
+			// Only master thread does this
+			nthreads = omp_get_num_threads();
+			printf("Number of threads = %d\n",
+				nthreads);
+		}
+	}
 }
---
+++ src/main_.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main_() {
+    std::cout << "FDTD C++ project!" << std::endl;
+}
