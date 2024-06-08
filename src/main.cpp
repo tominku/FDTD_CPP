ee2cf67ac7c976ae372c15ae6f8e58975b496ccb
tominku <tominku@gmail.com>
2024-06-09 00:23:47 +0900

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
 
-#define ROWS 10
-#define COLS 15 
+#define ROWS 5
+#define COLS 6 
 #define R_FI 0 // row first index
 #define R_LI (ROWS-1)  // row last index
 #define C_FI 0 // col first index
 #define C_LI (COLS-1)  // col last index
+#define NUM_THREADS 4
 
 int main()
 {
@@ -17,15 +21,35 @@ int main()
     double Hx[ROWS][COLS] = {0, };
     double Hy[ROWS][COLS] = {0, };
 
+    auto start = high_resolution_clock::now();
+    // Magnetic Field Update
+    #pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
     for (int i=R_FI; i<R_LI; i++)
     {
         for (int j=C_FI; j<C_LI; j++)
         {
             int udx = 1;
-            Hx[i][j] += udx * (Ez[i][j] - Ez[i][j+1]); 
-            Hy[][]
+            Hx[i][j] -= udx * (Ez[i][j] - Ez[i][j+1]); 
+            Hy[i][j] += udx * (Ez[i][j] - Ez[i+1][j]);
+            printf("M-Field i = %d, j= %d, threadId = %d \n", i, j, omp_get_thread_num());
         }
     }
+    // Electric Field Update
+    #pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+    for (int i=(R_FI+1); i<R_LI; i++)
+    {
+        for (int j=(C_FI+1); j<C_LI; j++)
+        {
+            Ez[i][j] += (Hy[i-1][j] - Hy[i][j]) + (Hx[i][j-1] - Hx[i][j]);
+            printf("E-Field i = %d, j= %d, threadId = %d \n", i, j, omp_get_thread_num());
+        }
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    
+    // To get the value of duration use the count()
+    // member function on the duration object
+    std::cout << "duration: " << duration.count() << " ms" << std::endl;
 
     /*
     int arr[4] = {9, };
