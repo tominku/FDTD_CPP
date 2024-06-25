5ffd9f2afe5385d3efa8a796c7c189880680ee1e
tominku <tominku@gmail.com>
2024-06-25 07:53:12 +0000

    ..

--- src/main.cu
+++ src/main.cu
@@ -22,11 +22,22 @@ public:
 
 };
 
+struct StructTest
+{
+    int a = 0;
+    int b = 1;
+};
+
 int main()
 {
     helloCUDA<<<1, 1>>>();
     Test test;
     test.Print();
+
+    StructTest st;
+    printf("struct test: %d \n", st.b);
+
     cudaDeviceSynchronize();        
+
     return 0;
 }
