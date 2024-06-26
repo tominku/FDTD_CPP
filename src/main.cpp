68a8d3a265e4b350679208266f4ec842b1dd9722
minku <tominku@gmail.com>
2024-06-27 00:05:40 +0900

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@ using namespace std::chrono;
 #define PRINT 0
 
 #define devisions_per_wave 10  // Divisions per Wavelength   [unitless]
-#define num_waves_x 80 //  # wave lengths in x-dir [unitless]
-#define num_waves_y 80 //  # wave lengths in y-dir 
+#define num_waves_x 48 //  # wave lengths in x-dir [unitless]
+#define num_waves_y 48 //  # wave lengths in y-dir 
 #define Nx (num_waves_x*devisions_per_wave + 1)
 #define Ny (num_waves_y*devisions_per_wave + 1)
 
