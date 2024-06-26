819e32ba27d5840141e71e50b9f0b52edcf4b82b
minku <tominku@gmail.com>
2024-06-26 13:47:46 +0900

    ..

--- src/main.cu
+++ src/main.cu
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
-__global__ void helloCUDA()
-{
-    printf("Hello, CUDA!\n");
-}
+#define N 10000000
+#define MAX_ERR 1e-6
 
 class Test
 {
@@ -27,21 +30,71 @@ struct StructTest
     int b = 1;
 };
 
+
+__global__ void helloCUDA()
+{
+    printf("Hello, CUDA!\n");
+}
+
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
 int main()
 {
     //gridDim()
-    dim3 grid_dim(8, 1, 1);
-    dim3 block_dim(512, 1, 1);
-        
-    //helloCUDA<<<grid_dim, block_dim>>>();
-    helloCUDA<<<5, 1>>>();
-    // Test test;
-    // test.Print();
+    //dim3 grid_dim(8, 1, 1);
+    //dim3 block_dim(512, 1, 1);
 
-    // StructTest st;
-    // printf("struct test: %d \n", st.b);
+    float *a, *b, *out;
+    float *d_a, *d_b, *d_out; 
 
-    cudaDeviceSynchronize();        
+    // Allocate host memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize host arrays
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f;
+        b[i] = 2.0f;
+    }
+
+    // Allocate device memory
+    cudaMalloc((void**)&d_a, sizeof(float) * N);
+    cudaMalloc((void**)&d_b, sizeof(float) * N);
+    cudaMalloc((void**)&d_out, sizeof(float) * N);
+
+    // Transfer data from host to device memory
+    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+
+    // Executing kernel 
+    vector_add<<<1,1>>>(d_out, d_a, d_b, N);
+    
+    // Transfer data back to host memory
+    cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
+
+    // Verification
+    for(int i = 0; i < N; i++){
+        assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
+    }
+
+    printf("Test");
+    printf("out[0] = %f\n", out[0]);
+    printf("PASSED\n");
+
+    // Deallocate device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+
+    // Deallocate host memory
+    free(a); 
+    free(b); 
+    free(out);
 
     return 0;
 }
