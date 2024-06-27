ce3e554e703d5fe50ada22f2785a3ae720642bd5
minku <tominku@gmail.com>
2024-06-27 16:41:52 +0900

    ..

--- src/main.cu
+++ src/main.cu
@@ -15,8 +15,8 @@
 #define MAX_ERR 1e-6
 
 #define devisions_per_wave 10  // Divisions per Wavelength   [unitless]
-#define num_waves_x 50 //  # wave lengths in x-dir [unitless]
-#define num_waves_y 50 //  # wave lengths in y-dir 
+#define num_waves_x 100 //  # wave lengths in x-dir [unitless]
+#define num_waves_y 100 //  # wave lengths in y-dir 
 #define Nx (num_waves_x*devisions_per_wave + 1)
 #define Ny (num_waves_y*devisions_per_wave + 1)
 #define N (Nx*Ny)
@@ -57,7 +57,7 @@ using namespace std;
 using value_t = float;
 
 ofstream output_file;
-bool do_logging = true;
+bool do_logging = false;
 
 #define ij_to_k(i, j, Nx) (Nx*(j) + (i)) 
 
