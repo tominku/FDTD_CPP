3e6f477ee8389f1605ee08bde375d6d19fb34c6c
minku <tominku@gmail.com>
2024-06-29 17:23:47 +0900

    ..

--- src/post_processing/vis2.py
+++ src/post_processing/vis2.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 path = "/home/minku/.data/output_cpu.txt"
 #path = "/home/minku/.data/output_matlab.txt"
@@ -8,17 +11,43 @@ info_line = output.readline()
 info = info_line.split(",")
 Nx = int(info[0])
 Ny = int(info[1])
+N = Nx * Ny
+
+material_image = np.zeros((Nx, Ny))
+path = "/home/minku/.data/output_material.txt"
+output = open(path, "r")
+material_values = output.read().split(',')
+#print(material_values)
+material_values_len = len(material_values)
+assert( N == material_values_len )
+print(f'N: {N}, material_values_len: {material_values_len}')
+for value, k in zip(material_values, range(len(material_values))):
+    val = float(value)
+    i = int(k % Nx)
+    j = int(k / Nx)
+    material_image[i, j] = val
+
+plt.imshow(material_image)
+#plt.show()
+
+
+path = "/home/minku/.data/output_cpu.txt"
+#path = "/home/minku/.data/output_matlab.txt"
+output = open(path, "r")
+info_line = output.readline()
+info = info_line.split(",")
+Nx = int(info[0])
+Ny = int(info[1])
 images = []
 steps = int(info[2])
 logging_period = int(info[3])
-N = Nx * Ny
 print(f'Nx: {Nx}, Ny: {Ny}, Nt: {steps}, logging_period: {logging_period}')
 min_value = 1e6
 max_value = -1e6
 frames = output.read().split(";")
 for frame in frames:
     image = np.zeros((Nx, Ny))
-    images.append(image)
+    #image = material_image.copy()
     frame_str_length = len(frame);
     if frame_str_length != 0:
         frame_values = frame.split(",");
@@ -36,22 +65,12 @@ for frame in frames:
                 max_value = val
     else:
         print(f'no frame: {frame_str_length}');
-                
-
-path = "/home/minku/.data/output_material.txt"
-output = open(path, "r")
-material_values = output.read().split(',')
-#print(material_values)
-material_values_len = len(material_values)
-assert( N == material_values_len )
-print(f'N: {N}, material_values_len: {material_values_len}')
+    
+    image = image + material_image
+    images.append(image)            
 
 
 print(f'min_value: {min_value}, max_value: {max_value}')
-
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 #fig = plt.figure( figsize=(12,12) )
 fig = plt.figure( figsize=(Ny / 15, Nx / 15) )
