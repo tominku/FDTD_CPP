e041c3606d21aa8a889f3f83a2f550608cf7cb50
minku <tominku@gmail.com>
2024-07-08 14:53:56 +0900

    ..

--- src/post_processing/vis_probes.py
+++ src/post_processing/vis_probes.py
@@ -25,20 +25,20 @@ with open(path, "r") as json_file:
 
 # print(f"dt: {dt}")
 # # plt.plot(em_probes[0]['data'], 'r-')
-# plt.plot(em_probes[0]['data'][1200:2000])
+#plt.plot(em_probes[2]['data'][1500:2000])
 # plt.plot(em_probes[1]['data'][1200:2000])
 # #plt.plot(em_probes[2]['data'][:1500])
 # #plt.plot(em_probes[2]['data'][:1500], 'o')
-# plt.show()
+#plt.show()
 
 # i_begin = 1200
 # i_end = 2000
-i_begin = 1600
+i_begin = 50
 i_end = 2000
 
 N = i_end - i_begin +1
 T = dt * (N - 1)
-signal = em_probes[0]['data'][i_begin:i_end]
+signal = em_probes[2]['data'][i_begin:i_end]
 spectrum = sptr.Spectrum(signal, T)
 spectrum.print_info()
 freq, fft_result = spectrum.compute()
