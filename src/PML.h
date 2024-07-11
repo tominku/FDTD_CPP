eb2a3f98f63739ab7aa88a0c8aac44d55973a123
minku <tominku@gmail.com>
2024-07-11 15:31:03 +0900

    ..

--- src/PML.h
+++ src/PML.h
@@ -14,23 +14,26 @@ struct PML_Node
     float c_E;
     float b_M;
     float c_M;    
+    float Q;
 };
 
 class PML
 {    
-private:
-    int n_PML_nodes_per_part;    
+private:    
     float order;
     float kappa_max;
     float sigma_max;
     float alpha_min;
     float alpha_max;
     PML_Node *part1;
-    PML_Node *part2;            
+    PML_Node *part2;     
+    void init_PML_part(PML_Node *PML_nodes, bool reverse_i, float idx_offset=0);       
     void init();
 
-public:    
-    PML(int n_PML_nodes_per_part_, float order_, float kappa_max_=2)
+public: 
+    int n_PML_nodes_per_part;       
+
+    PML(int n_PML_nodes_per_part_=20, float order_=3, float kappa_max_=2)
     {
         n_PML_nodes_per_part = n_PML_nodes_per_part_;        
         order = order_;        
@@ -48,30 +51,38 @@ public:
     }
 };
 
-void PML::init()
-{    
-    part1 = new PML_Node[n_PML_nodes_per_part];    
-    part2 = new PML_Node[n_PML_nodes_per_part];        
-
+void PML::init_PML_part(PML_Node *PML_nodes, bool reverse_i, float idx_offset=0)
+{
+    vector<float> vec_idx(n_PML_nodes_per_part);
     for (int i=0; i<n_PML_nodes_per_part; i++)
     {
+        if(reverse_i)
+            vec_idx[i] = (float)(n_PML_nodes_per_part - i);
+        else
+            vec_idx[i] = (float)(i + 1.0);
+    }
+
+    for (int i=0; i<=n_PML_nodes_per_part; ++i)
+    {
         PML_Node node;
-        node.idx = i;        
-        float p = powf(node.idx, order);
-        node.kappa_E = 1 + (kappa_max - 1) * p;
-        node.kappa_M = node.kappa_E;
-        node.sigma_E = sigma_max * p;
-        node.sigma_M = (mu0 / eps0) * node.sigma_E;
-        node.alpha_E  = alpha_min + (alpha_max-alpha_min)*(1 - p);
-        node.alpha_M  = (mu0 / eps0) * node.alpha_E;
+        node.idx = vec_idx[i] + idx_offset;        
+        float p_E = powf((node.idx-0.25) / (float)n_PML_nodes_per_part, order);
+        float p_M = powf((node.idx+0.25) / (float)n_PML_nodes_per_part, order);
+        node.kappa_E = 1 + (kappa_max - 1) * p_E;
+        node.kappa_M = 1 + (kappa_max - 1) * p_M;
+        node.sigma_E = sigma_max * p_E;
+        node.sigma_M = (mu0 / eps0) * sigma_max * p_M;
+        node.alpha_E  = alpha_min + (alpha_max-alpha_min)*(1 - p_E);
+        node.alpha_M  = (mu0 / eps0) * (alpha_min + (alpha_max-alpha_min)*(1 - p_M));
         float temp = node.kappa_E*eps0 + dt*(node.kappa_E*node.alpha_E + node.sigma_E);
         node.b_E = (node.kappa_E * eps0) / temp;
         node.c_E = (dt * node.sigma_E) / node.kappa_E * temp;
         temp = node.kappa_M*mu0 + dt*(node.kappa_M*node.alpha_M + node.sigma_M);
         node.b_M = (node.kappa_M * mu0) / temp;
         node.c_M = (dt * node.sigma_M) / node.kappa_M * temp;
-        
-        part1[i] = node;
+        node.Q = 0;
+
+        PML_nodes[i] = node;
 
         /*
         bex = (e0*kappa_e)./(e0*kappa_e+dt.*(o_pex+alpha_e.*kappa_e));
@@ -80,4 +91,12 @@ void PML::init()
         cmx = (-dt*o_pmx)./(dx*kappa_m.*(u0*kappa_m+dt.*(o_pmx+alpha_m.*kappa_m)));        
         */
     }
+}
+
+void PML::init()
+{    
+    part1 = new PML_Node[n_PML_nodes_per_part];  
+    init_PML_part(part1, true, 0);
+    part2 = new PML_Node[n_PML_nodes_per_part];      
+    init_PML_part(part2, false, 0);    
 }
