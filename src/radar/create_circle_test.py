296cf64363db7f7e482bc7d9b20ce051a423c0ed
tominku <tominku@gmail.com>
2024-07-23 01:06:46 +0900

    ..

--- src/radar/create_circle_test.py
+++ src/radar/create_circle_test.py
@@ -13,11 +13,11 @@ draw = ImageDraw.Draw(img)
 #draw.ellipse((x-r, y-r, x+r, y+r), fill=(255,0,0,0))
 #draw.
 radius = 1
-draw.circle((60, 70), radius, fill=50, width=0)
+draw.circle((50, 50), radius, fill=50, width=0)
 #draw.rectangle((30, 40, 60, 60), fill=50, width=0)
 pixel_value_to_eps = {50: 100, 30: 11.68}
 
-vec = np.array([0.2, 0.8]) - np.array([0.7, 0.6])
+vec = np.array([0.2, 0.8]) - np.array([0.5, 0.5])
 print(f'distance: {LA.norm(vec)} m')
 
 arr_img = np.array(img)
